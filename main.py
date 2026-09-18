de259921af71d8a427a1b66cab193e3e959a3a79
MeeshBOT <meesh.r113@gmail.com>
2026-09-19 02:35:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -50,14 +50,14 @@ COLOR_WARM_WHITE = 0xFFF9F5
 # ==========================================================
 # 4. باليت الصور والبنرات والـ Thumbnails
 # ==========================================================
-IMG_WELCOME_BANNER = "https://cdn.discordapp.com/attachments/1521473362373775481/1547882296122675210/35efc6dcbdf1fdfa3cb1848cf691847f.png?ex=6aa50939&is=6aa3b7b9&hm=58dd5372961d79d39beeae6a6f8162b605178fb653483204883060145bf164d3&"
+IMG_WELCOME_BANNER = "https://cdn.discordapp.com/attachments/1521473362373775481/1547882296122675210/35efc6dcbdf1fdfa3cb1848cf691847f.png"
 IMG_CHECK_BANNER   = None
-IMG_EMPTY_ICON     = "https://github.com/meeshr/MeeshBOT/blob/main/pinkloading.gif?raw=true"
+IMG_EMPTY_ICON     = "https://raw.githubusercontent.com/meeshr/MeeshBOT/main/pinkloading.gif"
 
-IMG_CLAIM_DEFAULT  = "https://github.com/meeshr/MeeshBOT/blob/main/frogconfetti.gif?raw=true"
-IMG_DELETE_ICON    = "https://github.com/meeshr/MeeshBOT/blob/main/trash%20.gif?raw=true"
-IMG_UNCLAIM_ICON   = "https://github.com/meeshr/MeeshBOT/blob/main/cry.gif?raw=true"
-IMG_CLEAR_ICON     = "https://github.com/meeshr/MeeshBOT/blob/main/clean.gif?raw=true"
+IMG_CLAIM_DEFAULT  = "https://raw.githubusercontent.com/meeshr/MeeshBOT/main/frogconfetti.gif"
+IMG_DELETE_ICON    = "https://raw.githubusercontent.com/meeshr/MeeshBOT/main/trash%20.gif"
+IMG_UNCLAIM_ICON   = "https://raw.githubusercontent.com/meeshr/MeeshBOT/main/cry.gif"
+IMG_CLEAR_ICON     = "https://raw.githubusercontent.com/meeshr/MeeshBOT/main/clean.gif"
 
 # ==========================================================
 # 5. الترقيم والإيموجيات
